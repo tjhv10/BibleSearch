2eb958304dd4573fbb9dd1b38cad39c5c5c2688b
tjhv10 <achiyabennatan@gmail.com>
2023-12-12 20:45:45 +0200

    Final version

--- bibleRefactor.py
+++ bibleRefactor.py
@@ -12,11 +12,180 @@ def find_book(file_path):
         text = file.read()
         matches = re.findall(r'\$:([\u0590-\u05FF]+(?: [\u0590-\u05FF]+)*)', text)
         return matches
+def split_numbers_to_rows(file_path):
+    # Read the file using UTF-8 encoding for Hebrew text
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-file_path = 'bibleH.txt'
-print( find_book(file_path))
+    # Function to split numbers with their text
+    def split_numbers(line):
+        # Using regular expression to find numbers and text after them
+        pattern = r'(\d+)([^0-9]*)'
+        parts = re.findall(pattern, line)
+
+        # Writing each number with its text in a new row
+        with open(file_path, 'a', encoding='utf-8') as file:
+            for number, text in parts:
+                file.write(f"{number}{text}\n")
+
+    # Apply the split_numbers function to each line
+    for line in lines:
+        split_numbers(line)
+
+def add_number_to_chapters(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        text = file.read()
+
+        # Find chapters without the first word
+        matches = re.findall(r'\b([\u0590-\u05FF]+(?: {2,}[\u0590-\u05FF]+)+)\b', text)
+
+        # Adding '1' with appropriate spacing to each chapter
+        for chapter in matches:
+            modified_chapter = re.sub(r'\b([\u0590-\u05FF]+)\b', r'\1 1', chapter, count=1)
+            text = text.replace(chapter, f'{modified_chapter}', 1)
+
+    # Write modified content back to the file
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(text)
+def print_number_string_pairs(file_path):
+    with open(file_path, 'r+', encoding='utf-8') as file:
+        text = file.read()
+
+        # Find number followed by string using regular expressions
+        matches = re.findall(r'(\d+)([\s\u0590-\u05FF()]+)(?=\d+|$)', text)
+        finish_string =''
+        for match in matches:
+            number, string_between = match
+            string_between = string_between.strip()
+            if string_between:  # Check if the string is not empty (ignoring spaces)
+                finish_string += f"{number}:{string_between}\n"
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(finish_string)
+
+def remove_empty_lines(file_name):
+    with open(file_name, 'r+',encoding='utf-8') as file:
+        lines = file.readlines()
+        file.seek(0)
+        file.truncate(0)
+        non_empty_lines = [line for line in lines if line.strip()]
+        file.writelines(non_empty_lines)
+def hebrew_to_gematria(hebrew_text):
+    gematria_values = {
+        'א': 1, 'ב': 2, 'ג': 3, 'ד': 4, 'ה': 5,
+        'ו': 6, 'ז': 7, 'ח': 8, 'ט': 9, 'י': 10,
+        'יא': 11, 'יב': 12, 'יג': 13, 'יד': 14, 'טו': 15,
+        'טז': 16, 'יז': 17, 'יח': 18, 'יט': 19, 'כ': 20,
+        'כא': 21, 'כב': 22, 'כג': 23, 'כד': 24, 'כה': 25,
+        'כו': 26, 'כז': 27, 'כח': 28, 'כט': 29
+        # Add more as needed for larger numbers
+    }
+
+    numerical_values = []
+    i = 0
+    while i < len(hebrew_text):
+        if i + 1 < len(hebrew_text) and hebrew_text[i:i+2] in gematria_values:
+            numerical_values.append(gematria_values[hebrew_text[i:i+2]])
+            i += 2
+        else:
+            numerical_values.append(gematria_values.get(hebrew_text[i], 0))
+            i += 1
+
+    return numerical_values
+
+def convert_hebrew_in_file(file_name):
+    with open(file_name, 'r+',encoding='utf-8') as file:
+        lines = file.readlines()
+        file.seek(0)
+        file.truncate(0)
+
+        for line in lines:
+            if len(line.strip()) <= 2:  # Check line length
+                modified_line = ''.join(str(num) for num in hebrew_to_gematria(line.strip()))
+                file.write(modified_line + '\n')
+            else:
+                file.write(line)
+def process_file(file_path):
+    with open(file_path, 'r+', encoding='utf-8') as file:
+        lines = file.readlines()
+        current_episode = ''
+        modified_lines = []
+
+        for line in lines:
+            line = line.strip()
+            if line.isnumeric():
+                current_episode = line
+            else:
+                modified_lines.append(f"{current_episode}:{line}")
+
+        file.seek(0)
+        file.truncate()
+        file.write('\n'.join(modified_lines))
+def remove_short_lines(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    filtered_lines = [line.strip() for line in lines if len(line.strip()) >= 4]
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write('\n'.join(filtered_lines))
+def add_number_to_file(file_path):
+    with open(file_path, 'r+', encoding='utf-8') as file:
+        content = file.read()
+        file.seek(0, 0)  # Move the cursor to the beginning of the file
+        file.write('1\n' + content)
+def delete_rows_from_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    lines = lines[1:-6]  # Remove the first row and the last six rows
+
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write(''.join(lines))
 
 
-  # Replace this with the path to your text file
-hebrew_words_with_two_spaces = find_chapter(file_path)
-print("Hebrew words with two spaces after them:", hebrew_words_with_two_spaces)
+def remove_vocalization_marks(input_text):
+    # Define the vocalization marks to be removed
+    vocalization_marks = "ְֱֲֳִֵֶַָׇֹֺֻּֽֿׁׂׅׄ׈׉׊׋׌׍׎׼׽׾׿"
+
+    # Remove the vocalization marks using regular expression
+    cleaned_text = re.sub(f"[{vocalization_marks}]", '', input_text)
+    return cleaned_text
+
+
+def remove_vocalization_from_file(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            text = file.read()
+            cleaned_text = remove_vocalization_marks(text)
+
+        with open(file_path, 'w', encoding='utf-8') as file:
+            file.write(cleaned_text)
+
+        print(f"Vocalization marks removed from '{file_path}' successfully.")
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+
+
+def get_strings_with_marker(file_path):
+    result_list = []
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            for line in lines:
+                    if line.startswith("$:"):
+                        result_list.append(line.split(':')[1].strip())
+
+        return result_list
+    except FileNotFoundError:
+        print(f"File '{file_path}' not found.")
+        return result_list
+file_path = 'NTtry.txt'
+print(get_strings_with_marker(file_path))
+# remove_vocalization_from_file(file_path)
+# delete_rows_from_file(file_path)
+# add_number_to_chapters(file_path)
+# print_number_string_pairs(file_path)
+# convert_hebrew_in_file(file_path)
+# add_number_to_file(file_path)
+# process_file(file_path)
+# remove_short_lines(file_path)
--- main.py
+++ main.py
@@ -34,7 +34,14 @@ def create_word_groups(num_of_words_in_each_group, text):
 
     return combine_lists_as_strings(word_groups)
 
+def extract_sublist(start_string, end_string,books):
+    start_index = books.index(start_string) if start_string in books else -1
+    end_index = books.index(end_string) if end_string in books else -1
 
+    if start_index != -1 and end_index != -1 and start_index <= end_index:
+        return books[start_index: end_index+1]
+    else:
+        return []
 def bestMatch(string, list_of_str, percent_of_accuracy):
     maxMatchWord = ''
     spm = 0
@@ -64,14 +71,7 @@ def highlight_substring(main_string, substring):
         print("Full Verse: " + main_string)
 
 
-def extract_sublist(start_string, end_string):
-    start_index = books.index(start_string) if start_string in books else -1
-    end_index = books.index(end_string) if end_string in books else -1
 
-    if start_index != -1 and end_index != -1 and start_index <= end_index:
-        return books[start_index: end_index+1]
-    else:
-        return []
 
 
 def search_in_bible(search_term, num_of_words, chosen_percent, chosen_books):
@@ -107,25 +107,150 @@ def search_in_bible(search_term, num_of_words, chosen_percent, chosen_books):
         print("File 'bible.txt' not found.")
         return results
 
+def search_in_English():
+    search_input = input("Enter the word, part of a verse, or a full verse to search for: ")
+    percent = input("Enter the percentage of accuracy to search for (90% is recommended): ")
+    print("\033[93mHere is the list of the books in the bible: \033[0m")
+    part_length = len(books) // 3
+    split_parts = [books[i:i + part_length] for i in range(0, len(books), part_length)]
+    for part in split_parts:
+        print(', '.join(part))
+    start_book = input("Enter the book that you want to start to search from. If you want the whole bible enter all. "
+                       "If you want the old testament enter ot and if you want the new testament enter nt: ")
+    end_book = ''
+    flag = False
+    if start_book == 'all':
+        start_book = 'Genesis'
+        end_book = 'Revelation'
+        flag = True
+    if start_book == 'ot':
+        start_book = 'Genesis'
+        end_book = 'Malachi'
+        flag = True
+    if start_book == 'nt':
+        start_book = 'Matthew'
+        end_book = 'Revelation'
+        flag = True
+    if not flag:
+        while start_book not in books:
+            start_book = input("This book is not in the books list, try again: ")
+        end_book = input("Enter the book that you want to end your search: ")
+        while end_book not in books:
+            end_book = input("This book is not in the books list, try again: ")
+    search_results = search_in_bible(search_input, count_words(search_input), percent, extract_sublist(start_book, end_book,books))
+    if search_results:
+        print(f"Results for '{search_input}':")
+        for result in search_results:
+            print(f"Book: {result[0]}, Chapter: {result[1]}, Verse: {result[2]}")
+            highlight_substring(result[3], result[4])
+    else:
+        print(f"No results found for '{search_input}' in the part of the bible that you chosen.")
 
-search_input = input("Enter the word, part of a verse, or a full verse to search for: ")
-percent = input("Enter the percentage of accuracy to search for (90% is recommended): ")
-print("\033[93mHere is the list of the books in the bible: \033[0m")
-part_length = len(books) // 3
-split_parts = [books[i:i + part_length] for i in range(0, len(books), part_length)]
-for part in split_parts:
-    print(', '.join(part))
-start_book = input("Enter the book that you want to start to search from: ")
-while start_book not in books:
-    start_book = input("This book is not in the books list, try again: ")
-end_book = input("Enter the book that you want to end your search: ")
-while end_book not in books:
-    end_book = input("This book is not in the books list, try again: ")
-search_results = search_in_bible(search_input, count_words(search_input), percent, extract_sublist(start_book, end_book))
-if search_results:
-    print(f"Results for '{search_input}':")
-    for result in search_results:
-        print(f"Book: {result[0]}, Chapter: {result[1]}, Verse: {result[2]}")
-        highlight_substring(result[3], result[4])
-else:
-    print(f"No results found for '{search_input}' in the part of the bible that you chosen.")
+booksH = ['בראשית', 'שמות', 'ויקרא', 'במדבר', 'דברים', 'יהושוע', 'שופטים', 'שמואל א', 'שמואל ב', 'מלכים א', 'מלכים ב', 'ישעיה',
+         'ירמיה', 'יחזקאל', 'הושע', 'יואל', 'עמוס', 'עובדיה', 'יונה', 'מיכה', 'נחום', 'חבקוק', 'צפניה', 'חגי', 'זכריה',
+         'מלאכי', 'תהילים', 'משלי', 'איוב', 'שיר השירים', 'רות', 'איכה', 'קהלת', 'אסתר', 'דניאל', 'עזרא', 'נחמיה',
+         'דברי הימים א', 'דברי הימים ב', 'מתי', 'מרקוס', 'לוקס', 'יוחנן', 'מעשי השליחים', 'אל הרומים', 'הראשונה אל הקורינתים',
+         'השניה אל הקורינתים', 'אל הגלטים', 'אל האפסים', 'אל הפיליפים', 'אל הקולוסים', 'הראשונה אל התסלוניקים',
+         'השניה אל התסלוניקים', 'הראשונה אל טימותיאוס', 'השניה אל טימותיאוס', 'אל טיטוס', 'אל פילימון', 'אל העברים', 'אגרת יעקב',
+         'הראשונה לכיפא', 'השניה לכיפא', 'הראשונה ליוחנן', 'השניה ליוחנן', 'השלישית ליוחנן', 'איגרת יהודה', 'התגלות']
+
+
+def search_in_bibleH(search_term, num_of_words, chosen_percent, chosen_books):
+    results = []
+    try:
+        with open("bibleH.txt", 'r',encoding='utf-8') as file:
+            flag = False
+            lines = file.readlines()
+            for line in lines:
+                if line.startswith('$:'):
+                    current_book = line.split(':')[1].strip()
+                    if current_book not in chosen_books:
+                        flag = False
+                        continue
+                    else:
+                        flag = True
+                else:
+                    if not flag:
+                        continue
+                    verse_text = line.strip()
+                    verse_parts_list = create_word_groups(num_of_words, verse_text)
+                    matchedPart = bestMatch(search_term, verse_parts_list, chosen_percent)
+                    if matchedPart == '':
+                        continue
+                    current_verse = verse_text.split()[0].split(':')[1]
+                    current_chapter = verse_text.split()[0].split(':')[0]
+                    words = verse_text.split()
+                    results.append((current_book, current_chapter, current_verse, ' '.join(words[1:]), matchedPart))
+
+        return results
+
+    except FileNotFoundError:
+        print("File 'bibleH.txt' not found.")
+        return results
+
+def highlight_substringH(main_string, substring):
+    start_index = main_string.find(substring)
+
+    if start_index != -1:
+        end_index = start_index + len(substring.strip(",.:;"))
+        highlighted_string = (
+                main_string[:start_index]
+                + "\033[93m" + main_string[start_index:end_index]
+                + "\033[0m"
+                + main_string[end_index:]
+        )
+        print("פסוק: " + highlighted_string)
+    else:
+        print("פסוק: " + main_string)
+
+def search_in_Hebrew():
+    flag = False
+    search_input = input("הכנס פסוק, חלק מפסוק או מילה: ")
+    percent = input("הכנס אחוזי דיוק (90% הכי מומלץ): ")
+    print("\033[93mהנה רשימה של ספרי התנך:\033[0m")
+    part_length = len(booksH) // 4
+    split_parts = [booksH[i:i + part_length] for i in range(0, len(booksH), part_length)]
+    for part in split_parts:
+        print(', '.join(part))
+    start_book = input("הכנס ספר שאתה רוצה להתחיל לחפש ממנו. אם אתה רוצה את כל הכתובים הכנס הכל."
+                       " אם אתה רוצה את כל התנך הכנס תנך. אם אתה רוצה את כל הברית החדשה הכנס בח: ")
+    end_book = ''
+    if start_book =='הכל':
+        start_book = 'בראשית'
+        end_book = 'התגלות'
+        flag = True
+    if start_book =='תנך':
+        start_book = 'בראשית'
+        end_book = 'דברי הימים ב'
+        flag = True
+    if start_book =='בח':
+        start_book = 'מתי'
+        end_book = 'התגלות'
+        flag = True
+    if not flag:
+        while start_book not in booksH:
+            start_book = input("הספר לא קיים, נסה שוב: ")
+        end_book = input("הכנס ספר שאתה רוצה לסיים לחפש אחריו: ")
+        while end_book not in booksH:
+            end_book = input("הספר לא קיים, נסה שוב: ")
+    search_results = search_in_bibleH(search_input, count_words(search_input), percent,extract_sublist(start_book, end_book,booksH))
+    if search_results:
+        print(f'תוצאות בשביל "{search_input}" בחלק של הכתובים שבחרת')
+        for result in search_results:
+            print(f"ספר: {result[0]}, פרק: {result[1]}, פסוק: {result[2]}")
+            highlight_substringH(result[3], result[4])
+    else:
+        print(f'אין תוצאות בשביל "{search_input}" בחלק של הכתובים שבחרת')
+while 1:
+    flag = False
+    lang = input("האם אתה רוצה לחפש בעברית או אנגלית? בשביל עברית הכנס ע ובשביל אנגלית הכנס א. אם ברצונך לסיים הכנס ס: ")
+    if lang =='ע':
+        search_in_Hebrew()
+        flag = True
+    if lang == 'א':
+        search_in_English()
+        flag = True
+    if lang == 'ס':
+        break
+    if not flag:
+        input("לא בחרת שפה נכונה, לחץ אנטר ונסה שוב")
